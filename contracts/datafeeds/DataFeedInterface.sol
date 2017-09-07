57b3c7a678c218b785d0b9cc25cdb3688299f6fa
Travis Jacobs <trav221@gmail.com>
2017-09-07 16:03:01 +0200

    Fix function visibility

--- contracts/datafeeds/DataFeedInterface.sol
+++ contracts/datafeeds/DataFeedInterface.sol
@@ -15,6 +15,17 @@ contract DataFeedInterface is AssetRegistrarInterface {
 
     // CONSTANT METHODS
 
+    // Get registartion specific information
+    function isDataSet(address ofAsset) constant returns (bool) {}
+    function isDataValid(address ofAsset) constant returns (bool) {}
+    function numRegisteredAssets() constant returns (uint) {}
+    function getRegisteredAssetAt(uint id) constant returns (address) {}
+    // Get asset specific information
+    function getName(address ofAsset) constant returns (string) {}
+    function getSymbol(address ofAsset) constant returns (string) {}
+    function getDecimals(address ofAsset) constant returns (uint256) {}
+    function getDescriptiveInformation(address ofAsset) constant returns (string, string, uint256, string, bytes32) {}
+    function getSpecificInformation(address ofAsset) constant returns (uint256, bytes32, address, address) {}
     // Get data feed specific information
     function getQuoteAsset() constant returns (address) {}
     function getInterval() constant returns (uint) {}
