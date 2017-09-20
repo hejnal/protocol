d6efb93287264f392f9ee0eb22522c3aa5da414c
Travis Jacobs <trav221@gmail.com>
2017-09-20 17:04:52 +0200

    Convert deploy script to es6 syntax - add temporary deploy script to package.json

--- deploy.js
+++ deploy.js
@@ -1,37 +1,26 @@
-#!/usr/bin/env node
-
 const artifactor = require('./artifactor.js');
 const conf = require('./conf.js');    // TODO: specify alternative confs as CLI input?
 const deployer = require('./deployer.js');
 const tokenInfo = require('./migrations/config/token_info.js');
 
-function main() {
+async function main() {
   //addressBook = artifacts[NETWORK];
   if(conf.network === 'kovan') {
     let artifacts = artifactor.load();
     const mlnAddr = tokenInfo[conf.network].find(t => t.symbol === 'MLN-T').address;
-    deployer.link('Vault', 'rewards');
-    deployer.deploy('DataFeed', [mlnAddr, 120, 60])
-    .then(address => {addressBook['DataFeed'] = address})
-    .then(() => deployer.deploy('SimpleMarket'))
-    .then(address => {addressBook['SimpleMarket'] = address})
-    .then(() => deployer.deploy('Sphere', [addressBook.DataFeed, addressBook.SimpleMarket]))
-    .then(address => {addressBook['Sphere'] = address})
-    .then(() => deployer.deploy('Participation'))
-    .then(address => {addressBook['Participation'] = address})
-    .then(() => deployer.deploy('RMMakeOrders'))
-    .then(address => {addressBook['RMMakeOrders'] = address})
-    .then(() => deployer.deploy('Governance'))
-    .then(address => {addressBook['Governance'] = address})
-    .then(() => deployer.deploy('calculate'))
-    .then(address => {addressBook['calculate'] = address})
-    .then(() => deployer.deploy('rewards'))
-    .then(address => {addressBook['rewards'] = address})
+    //deployer.link('Vault', 'rewards');
+    addressBook['DataFeed'] = await deployer.deploy('DataFeed', [mlnAddr, 120, 60]);
+    addressBook['SimpleMarket'] = await deployer.deploy('SimpleMarket')
+    addressBook['Sphere'] = await deployer.deploy('Sphere', [addressBook.DataFeed, addressBook.SimpleMarket])
+    addressBook['Participation'] = await deployer.deploy('Participation');
+    addressBook['RMMakeOrders'] = await deployer.deploy('RMMakeOrders');
+    addressBook['Governance'] = await deployer.deploy('Governance');
+    addressBook['calculate'] = await deployer.deploy('calculate');
+    addressBook['rewards'] = await deployer.deploy('rewards');
     // TODO: link libraries with Vault and Version
-    .then(() => {
-      artifacts[conf.network] = addressBook;
-      artifactor.save(artifacts);
-    })
+    // TODO: register assets
+    artifacts[conf.network] = addressBook;
+    artifactor.save(artifacts);
   }
 }
 
