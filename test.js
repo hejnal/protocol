812717ea5aa131d83f5ab4aac86d13bd1949db01
Travis Jacobs <trav221@gmail.com>
2017-09-20 14:30:34 +0200

    Bootstrap es6 syntax in test

--- test.js
+++ test.js
@@ -1,3 +1,5 @@
+import 'babel-polyfill';
+
 const api = require('./api.js').getApi();
 const artifactor = require('./artifactor.js');
 const chai = require('chai');
@@ -7,11 +9,12 @@ const net = 'test';
 
 describe('Assets', () => {
   let mlnToken;
-  before('Deploy asset', () => {
-    return deployer.deploy('PreminedAsset', ['Melon', 'MLN-T', 18, 10 ** 28])
-    .then(() => console.log('abc'))
+  before('Deploy asset', async () => {
+    mlnToken = await deployer.deploy('PreminedAsset', ['Melon', 'MLN-T', 18, 10 ** 28]);
+    console.log('abc');
   });
 
-  it.skip('Should have correct amount of premined tokens', () => {
+  it.skip('Should have correct amount of premined tokens', async () => {
+    assert.equal(await mlnToken.balanceOf(accounts[0]), PREMINED)
   });
 });
