7bd84535c10d6d1294d33992744d9053ffd3ae57
Travis Jacobs <trav221@gmail.com>
2017-09-20 10:11:25 +0200

    Improve search for contract file and its contents - no more finding doubly-produced files

--- deploy.js
+++ deploy.js
@@ -22,23 +22,24 @@ const api = new Api(transport);
 const outDir = path.join(__dirname, 'out');
 const NETWORK = 'kovan';
 
-// returns path to the contract file defined by regexp
-// TODO: watch for case where Dapp outputs files in weird subdirs (is it possible to catch?)
-function getFileContent(regexp){
-  const filePath = klawSync(outDir, {filter: f => f.path.match(regexp)})[0];
-  return fs.readFileSync(filePath, 'utf8');
+// returns content of a contract file as Object
+function getSolcOutput(contract) {
+  const reJson = new RegExp(`.*\/${contract}\.sol.json$`);
+  const reSol = new RegExp(`.*\/?${contract}\.sol$`);
+  const filePath = klawSync(outDir, {filter: f => f.path.match(reJson)})[0].path;
+  const content = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+  const sourcepath = content.sourceList.filter(e => e.match(reSol))[0];
+  return content.contracts[`${sourcepath}:${contract}`];
 }
 
 // returns string
 function getBytecode(contract) {
-  const re = new RegExp(`.*${contract}\.bin$`);
-  return getFileContent(re);
+  return getSolcOutput(contract).bin;
 }
 
 // returns Object
 function getAbi(contract) {
-  const re = new RegExp(`.*${contract}\.abi$`);
-  return JSON.parse(getFileContent(re));
+  return JSON.parse(getSolcOutput(contract).abi);
 }
 
 // contract name and filename needs to have same case-sensitive name
@@ -52,39 +53,43 @@ function deployContract(name, args) {
 
 //XXX: link using this until dapp does it by default (dapphub/dapp#55)
 function link(fromContract, toContract) {
-  const addr = artifactor.getArtifact('rewards', NETWORK);
+  const libAddr = artifactor.getArtifact(toContract, NETWORK);
   let bytecode = getBytecode(fromContract);
   const libs = {};
-  libs[toContract] = addr;
+  libs[toContract] = libAddr;
   bytecode = solc.linkBytecode(bytecode, libs);
-  console.log(bytecode);
 }
 
 function main() {
-  let artifacts = artifactor.load();
-  const mlnAddr = tokenInfo[NETWORK].find(t => t.symbol === 'MLN-T').address;
-  link('Vault', 'rewards');
-  return;
-  deployContract('datafeeds/DataFeed', [mlnAddr, 120, 60])
-  .then(address => {artifacts.kovan['DataFeed'] = address})
-  .then(() => deployContract('exchange/SimpleMarket'))
-  .then(address => {artifacts.kovan['SimpleMarket'] = address})
-  .then(() => deployContract('sphere/Sphere', [artifacts.kovan.DataFeed, artifacts.kovan.SimpleMarket]))
-  .then(address => {artifacts.kovan['Sphere'] = address})
-  .then(() => deployContract('participation/Participation'))
-  .then(address => {artifacts.kovan['Participation'] = address})
-  .then(() => deployContract('riskmgmt/RMMakeOrders'))
-  .then(address => {artifacts.kovan['RMMakeOrders'] = address})
-  .then(() => deployContract('governance/Governance'))
-  .then(address => {artifacts.kovan['Governance'] = address})
-  .then(() => deployContract('libraries/calculate'))
-  .then(address => {artifacts.kovan['calculate'] = address})
-  .then(() => deployContract('libraries/rewards'))
-  .then(address => {artifacts.kovan['rewards'] = address})
-  .then(() => deployContract('libraries/rewards'))
-  .then(address => {artifacts.kovan['rewards'] = address})
-  // TODO: link libraries with Vault and Version
-  .then(() => artifactor.save(artifacts))
+  addressBook = artifacts[NETWORK];
+  if(NETWORK === 'kovan') {
+    let artifacts = artifactor.load();
+    const mlnAddr = tokenInfo[NETWORK].find(t => t.symbol === 'MLN-T').address;
+    link('Vault', 'rewards');
+    deployContract('DataFeed', [mlnAddr, 120, 60])
+    .then(address => {addressBook['DataFeed'] = address})
+    .then(() => deployContract('SimpleMarket'))
+    .then(address => {addressBook['SimpleMarket'] = address})
+    .then(() => deployContract('Sphere', [addressBook.DataFeed, addressBook.SimpleMarket]))
+    .then(address => {addressBook['Sphere'] = address})
+    .then(() => deployContract('Participation'))
+    .then(address => {addressBook['Participation'] = address})
+    .then(() => deployContract('RMMakeOrders'))
+    .then(address => {addressBook['RMMakeOrders'] = address})
+    .then(() => deployContract('Governance'))
+    .then(address => {addressBook['Governance'] = address})
+    .then(() => deployContract('calculate'))
+    .then(address => {addressBook['calculate'] = address})
+    .then(() => deployContract('rewards'))
+    .then(address => {addressBook['rewards'] = address})
+    .then(() => deployContract('rewards'))
+    .then(address => {addressBook['rewards'] = address})
+    // TODO: link libraries with Vault and Version
+    .then(() => {
+      artifacts[NETWORK] = addressBook;
+      artifactor.save(artifacts);
+    })
+  }
 }
 
 if(require.main === module) {
