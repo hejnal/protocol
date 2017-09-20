699c24b2ce8d9b333ccdbab2b7f6f4d1f0dece4d
Travis Jacobs <trav221@gmail.com>
2017-09-20 10:31:50 +0200

    Abstract deployer into module - add config file

--- deploy.js
+++ deploy.js
@@ -10,8 +10,7 @@
 // modify the artifacts file
 
 const artifactor = require('./artifactor.js');
-const fs = require('fs');
-const klawSync = require('klaw-sync');
+const deployer = require('./deployer.js');
 const path = require('path');
 const solc = require('solc');
 const tokenInfo = require('./migrations/config/token_info.js');
@@ -20,73 +19,33 @@ const transport = new Api.Transport.Http('http://localhost:8545');
 const api = new Api(transport);
 
 const outDir = path.join(__dirname, 'out');
-const NETWORK = 'kovan';
-
-// returns content of a contract file as Object
-function getSolcOutput(contract) {
-  const reJson = new RegExp(`.*\/${contract}\.sol.json$`);
-  const reSol = new RegExp(`.*\/?${contract}\.sol$`);
-  const filePath = klawSync(outDir, {filter: f => f.path.match(reJson)})[0].path;
-  const content = JSON.parse(fs.readFileSync(filePath, 'utf8'));
-  const sourcepath = content.sourceList.filter(e => e.match(reSol))[0];
-  return content.contracts[`${sourcepath}:${contract}`];
-}
-
-// returns string
-function getBytecode(contract) {
-  return getSolcOutput(contract).bin;
-}
-
-// returns Object
-function getAbi(contract) {
-  return JSON.parse(getSolcOutput(contract).abi);
-}
-
-// contract name and filename needs to have same case-sensitive name
-function deployContract(name, args) {
-  if(args === undefined) args = [];
-  const abi = getAbi(name);
-  const bytecode = getBytecode(name);
-  const contract = api.newContract(abi);
-  return contract.deploy({data: bytecode}, args);
-}
-
-//XXX: link using this until dapp does it by default (dapphub/dapp#55)
-function link(fromContract, toContract) {
-  const libAddr = artifactor.getArtifact(toContract, NETWORK);
-  let bytecode = getBytecode(fromContract);
-  const libs = {};
-  libs[toContract] = libAddr;
-  bytecode = solc.linkBytecode(bytecode, libs);
-}
+const conf = require('./conf.js');    // TODO: specify alternative confs as CLI input?
 
 function main() {
-  addressBook = artifacts[NETWORK];
-  if(NETWORK === 'kovan') {
+  //addressBook = artifacts[NETWORK];
+  if(conf.network === 'kovan') {
     let artifacts = artifactor.load();
-    const mlnAddr = tokenInfo[NETWORK].find(t => t.symbol === 'MLN-T').address;
-    link('Vault', 'rewards');
-    deployContract('DataFeed', [mlnAddr, 120, 60])
+    const mlnAddr = tokenInfo[conf.network].find(t => t.symbol === 'MLN-T').address;
+    deployer.link('Vault', 'rewards');
+    deployer.deploy('DataFeed', [mlnAddr, 120, 60])
     .then(address => {addressBook['DataFeed'] = address})
-    .then(() => deployContract('SimpleMarket'))
+    .then(() => deployer.deploy('SimpleMarket'))
     .then(address => {addressBook['SimpleMarket'] = address})
-    .then(() => deployContract('Sphere', [addressBook.DataFeed, addressBook.SimpleMarket]))
+    .then(() => deployer.deploy('Sphere', [addressBook.DataFeed, addressBook.SimpleMarket]))
     .then(address => {addressBook['Sphere'] = address})
-    .then(() => deployContract('Participation'))
+    .then(() => deployer.deploy('Participation'))
     .then(address => {addressBook['Participation'] = address})
-    .then(() => deployContract('RMMakeOrders'))
+    .then(() => deployer.deploy('RMMakeOrders'))
     .then(address => {addressBook['RMMakeOrders'] = address})
-    .then(() => deployContract('Governance'))
+    .then(() => deployer.deploy('Governance'))
     .then(address => {addressBook['Governance'] = address})
-    .then(() => deployContract('calculate'))
+    .then(() => deployer.deploy('calculate'))
     .then(address => {addressBook['calculate'] = address})
-    .then(() => deployContract('rewards'))
-    .then(address => {addressBook['rewards'] = address})
-    .then(() => deployContract('rewards'))
+    .then(() => deployer.deploy('rewards'))
     .then(address => {addressBook['rewards'] = address})
     // TODO: link libraries with Vault and Version
     .then(() => {
-      artifacts[NETWORK] = addressBook;
+      artifacts[conf.network] = addressBook;
       artifactor.save(artifacts);
     })
   }
---
+++ deployer.js
@@ -0,0 +1,48 @@
+const artifactor = require('./artifactor');
+const conf = require('./conf.js');
+const fs = require('fs');
+const klawSync = require('klaw-sync');
+const path = require('path');
+
+// returns content of a contract file as Object
+function getSolcOutput(contract) {
+  const reJson = new RegExp(`.*\/${contract}\.sol.json$`);
+  const reSol = new RegExp(`.*\/?${contract}\.sol$`);
+  const filePath = klawSync(outDir, {filter: f => f.path.match(reJson)})[0].path;
+  const content = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+  const sourcepath = content.sourceList.filter(e => e.match(reSol))[0];
+  return content.contracts[`${sourcepath}:${contract}`];
+}
+
+// returns string
+function getBytecode(contract) {
+  return getSolcOutput(contract).bin;
+}
+
+// returns Object
+function getAbi(contract) {
+  return JSON.parse(getSolcOutput(contract).abi);
+}
+
+// contract name and filename needs to have same case-sensitive name
+function deploy(name, args) {
+  if(args === undefined) args = [];
+  const abi = getAbi(name);
+  const bytecode = getBytecode(name);
+  const contract = api.newContract(abi);
+  return contract.deploy({data: bytecode}, args);
+}
+
+//XXX: link using this until dapp does it by default (dapphub/dapp#55)
+function link(fromContract, toContract) {
+  const libAddr = artifactor.getArtifact(toContract, conf.network);
+  let bytecode = getBytecode(fromContract);
+  const libs = {};
+  libs[toContract] = libAddr;
+  bytecode = solc.linkBytecode(bytecode, libs);
+}
+
+module.exports = {
+  deploy,
+  link
+}
